96e47ecb394377c4d4e33fc68b4da124ad430b8d
fabi-cm <dahofcmmejia@gmail.com>
2023-06-01 09:22:00 -0400

    TDD SI NO SE INGRESA UN MONTO SE LE PEDIRA QUE PONGA UNO

--- src/cajero.test.js
+++ src/cajero.test.js
@@ -4,4 +4,7 @@ describe("Cajero", () => {
   it("Ingrasar un monto de venta deberia devolver su valor", () => {
     expect(cajero(3)).toEqual(3);
   });
+  it("Si no se ingresa un monto de venta devuelve un mensaje: Ingrese el monto", () => {
+    expect(cajero("")).toBe("Ingrese un monto")
+  })
 });
